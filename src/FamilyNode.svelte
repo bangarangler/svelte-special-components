0455931d6fd2daadbe415cad5058157df9b306c7
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-07 19:53:56 -0400

    <script context="module"></script> svelte use case example

--- src/FamilyNode.svelte
+++ src/FamilyNode.svelte
@@ -1,14 +1,39 @@
+<script context="module">
+  console.log('runs once!')
+
+  let deactivateNode;
+</script>
+
 <script>
+  console.log('runs multiple times')
+
+  function deactivate() {
+    isActive = false;
+  }
   export let member;
+
+  let isActive;
+
+  function activate() {
+    if (deactivateNode) {
+      deactivateNode()
+    }
+    isActive = true;
+    deactivateNode = deactivate;
+  }
 </script>
 
 <style>
   div {
     margin-left: 2rem;
   }
+
+  .active {
+    color: red;
+  }
 </style>
 
-<div>
+<div on:click={activate} class:active={isActive}>
   <h1>{member.name}</h1>
   {#if member.isParent}
     {#each member.children as child}
